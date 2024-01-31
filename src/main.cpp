3436496f3d5e55663d7f93d8955324dab1275828
antoine guerrin <antoine.guerrin@edu.ece.fr>
2024-01-31 13:06:10 +0100

    Etape 3 validée

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <esp_sleep.h>
 
 // Define the pins that we will use
 #define DHTPIN 33 // Digital pin connected to the DHT sensor 
-#define LED 26
 #define DHTTYPE DHT11 // DHT 11
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
@@ -13,67 +13,41 @@ uint32_t previousMillis = 0;
 const long interval = 5000;  // Interval de 5 secondes en millisecondes
 
 void setup() {
-  pinMode(LED, OUTPUT);
   Serial.begin(9600);
   // Initialize device.
   dht.begin();
-  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  } else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  } else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
 
-  // Print temperature sensor details.
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
-  Serial.println(F("------------------------------------"));
-
-  // Print humidity sensor details.
-  dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
+  // Mise en veille profonde (deep sleep) pendant 5 secondes
+  Serial.println("Démarrage de la mise en veille...");
+  Serial.println();
+  esp_sleep_enable_timer_wakeup(interval * 1000); // Conversion en microsecondes
+  delay(10);
+  Serial.flush();
+  
+  esp_deep_sleep_start();
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    // Enregistrer le dernier moment où la mesure a été effectuée
-    previousMillis = currentMillis;
-
-    // Get temperature event and print its value.
-    sensors_event_t event;
-    dht.temperature().getEvent(&event);
-    if (isnan(event.temperature)) {
-      Serial.println(F("Erreur de lecture de la température !"));
-    }
-    else {
-      Serial.print(F("Température: "));
-      Serial.print(event.temperature);
-      Serial.println(F("°C"));
-    }
-    // Get humidity event and print its value.
-    dht.humidity().getEvent(&event);
-    if (isnan(event.relative_humidity)) {
-      Serial.println(F("Erreur de lecture de l'humidité !"));
-    }
-    else {
-      Serial.print(F("Humidité: "));
-      Serial.print(event.relative_humidity);
-      Serial.println(F("%"));
-      Serial.println();
-    }
-  }
+  // La fonction loop est vide car nous utilisons la mise en veille profonde
 }
 
 
